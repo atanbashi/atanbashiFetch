71bfd410cc2211b8dfb7627aa3e87caa06991c79
Радик Мустафин <radek_10_86@mail.ru>
2021-01-28 13:05:24 +0300

    done

--- script2.js
+++ script2.js
@@ -1,74 +1,72 @@
-let body = document.body;
+let url = window.location.toString();
 
-let date = new Date();
+let preloader = document.getElementById("preload");
 
-let requestFromPromise, dateFromPromise;
+let newDate = new Date();
 
-let getDate = new Promise((resolve, reject) => {
-  setTimeout(() => date ? resolve(date) : reject('Time didnt find'), 2000)
+let getUserName = (url) => {
+  let urlSplit = url.split('=');
+  let userName = urlSplit[1];
+  if (userName == undefined) {
+    userName = 'atanbashi';
+  }
+  return userName;
+}
+
+let name = getUserName(url);
+
+let getNewDate = new Promise((resolve, reject) => {
+  setTimeout(() => newDate ? resolve(newDate) : reject('Time didnt find'), 2000)
 });
 
-let getUserName = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    let url = window.location.toString();
-    let getUrl = url.split('=');
-    let name = getUrl[1];
-    resolve(name == 'undefined' ? name : 'atanbashi');
-  }, 3000);
-});
+let getUserRequest =  fetch('https://api.github.com/users/' + name);
 
-let initialUser = (json) => {
-  let avatar = json.avatar_url;
-  let name = json.name;
-  let userInfo = json.bio;
-  let url = json.url;
-}
-
-let createName = () => {
-  let profileName = document.createElement('h1');
-  profileName.innerHTML = name;
-  body.appendChild(profileName);
-}
-
-let createUserInfo = () => {
-  let profileInfo = document.createElement('p');
-  profileInfo.innerHTML = userInfo;
-  body.appendChild(profileInfo);
-}
-
-let createUserAvatar = () => {
-  let userAvatar = document.createElement('img');
-  let newString = document.createElement('br');
-  userAvatar.src = avatar;
-  body.appendChild(userAvatar);
-  body.appendChild(newString);
-}
-
-let createDate = () => {
-  let currentDate = document.createElement('p');
-  currentDate.innerHTML = dateFromPromise;
-  body.appendChild(currentDate);
-}
-
-let maskPreload = () => {
-  let preloader = document.querySelector('.container');
-  preloader.style.display = 'none';
-}
-
-Promise.all([getUserName, getDate]
-  .then(([name, date]) => {
-      dateFromPromise = date;
-      requestFromPromise = name;
-      return fetch('https://api.github.com/users/' + name);
+Promise.all([getUserRequest, getNewDate])
+  .then(([userInfoRequest, requestNewDate]) => {
+      userRequest = userInfoRequest;
+      requestDate = requestNewDate;
   })
-  .then(res => res.json())
+  .then(res => userRequest.json())
   .then(json => {
-    initialUser(json);
-    maskPreload();
-    createName();
-    createUserInfo();
-    createUserAvatar();
-    createDate();
-  })
-  )
-  .catch(err => alert('Информация о пользователе не доступна'));
+    let avatar = json.avatar_url;
+    let name = json.name;
+    let userInfo = json.bio;
+    let profile = json.html_url;
+    if (name) {
+      let createUserAvatar = () => {
+        let addAvatar = document.createElement('img');
+        let newString = document.createElement('br');
+        addAvatar.src = avatar;
+        document.body.appendChild(addAvatar);
+        document.body.appendChild(newString);
+      }
+
+      let createUserInfo = () => {
+        let profileInfo = document.createElement('p');
+        profileInfo.innerHTML = userInfo;
+        document.body.appendChild(profileInfo);
+      }
+
+      let createName = () => {
+        let profileName = document.createElement('h1');
+        profileName.innerHTML = name;
+        document.body.appendChild(profileName);
+      }
+
+      let createNewDate = () => {
+        let currentDate = document.createElement('h3');
+        currentDate.innerHTML = requestDate;
+        document.body.appendChild(currentDate);
+      }
+
+      preloader.style.display = 'none';
+      createUserAvatar();
+      createName();
+      createUserInfo();
+      createNewDate();
+  } else {
+    alert('Информация о пользователе не доступна');
+  }
+})
+
+.catch(err => alert(err + ' Информация о пользователе не доступна'));
